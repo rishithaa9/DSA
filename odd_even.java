5c24f9addd8d34c63787cea453d0846b10108367
rishithaa9 <cb.ai.u4aid23023@cb.students.amrita.edu>
2025-11-23 16:18:40 +0530

    Odd Even

---
+++ odd_even.java
@@ -0,0 +1,12 @@
+public class odd_even{
+    public static void main(String[] arg){
+        int n=11;
+
+        if(n%2==0){
+            System.out.println("Even");
+        }
+        else {
+            System.out.println("Odd");
+        }
+    }
+}
