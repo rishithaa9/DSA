dfc6e4bf4ae75fd6697f2bd0455de23ae763f9d6
rishithaa9 <cb.ai.u4aid23023@cb.students.amrita.edu>
2025-12-08 14:17:14 +0530

    Positive & Negative

---
+++ postive_neg.java
@@ -0,0 +1,12 @@
+public class postive_neg {
+    public static void main(String[] args){
+        int n=-5;
+        if(n>0){
+            System.out.println("positive");
+        }
+        else {
+            System.out.println("Negative");
+        }
+    }
+    
+}
