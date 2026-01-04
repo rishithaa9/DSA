8b0ad6eb4e02cc132f992d0cb13767d376390bbb
rishithaa9 <cb.ai.u4aid23023@cb.students.amrita.edu>
2026-01-04 10:20:47 +0530

    551

---
+++ 551.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        if s.count('A') > 1:
+            return False
+        if "LLL" in s:
+            return False
+        
+        return True
