4edfec200b7670b550c7730c6a244494cfdb3423
rishithaa9 <cb.ai.u4aid23023@cb.students.amrita.edu>
2026-01-19 08:09:09 +0530

    Count Words

---
+++ 2085.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countWords(self, words1: List[str], words2: List[str]) -> int:
+        count1={}
+        count2={}
+        for i in words1:
+            if i in count1:
+                count1[i]+=1
+            else:
+                count1[i]=1
+        for j in words2:
+            if j in count2:
+                count2[j]+=1
+            else:
+                count2[j]=1
+
+        ans=0
+        for i,freq in count1.items():
+            if freq==1 and count2.get(i,0)==1:
+                ans+=1
+        return ans 
+            
+
+
+
+            
