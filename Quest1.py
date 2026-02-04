6c7a8fdbee9f8ab1a10ab7835dc3fb7b202f8d9e
rishithaa9 <cb.ai.u4aid23023@cb.students.amrita.edu>
2026-02-04 21:14:44 +0530

    Array

---
+++ Quest1.py
@@ -0,0 +1,12 @@
+#1929
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        result=[]
+        for i in nums:
+            result.append(i)
+        for i in nums:
+            result.append(i)
+    
+
+        return result
+        
