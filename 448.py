1339ec5cda87df5a83c9efe15af6995be4ed3a81
rishithaa9 <cb.ai.u4aid23023@cb.students.amrita.edu>
2026-02-08 08:54:14 +0530

    Best

---
+++ 448.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        nums_set=set(nums)
+        ans=[]
+
+        for i in range(1, len(nums) + 1):
+            if i not in nums_set:   #
+                ans.append(i)
+        return ans
