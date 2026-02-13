0a4246cf124304eae15bcdc2959c47476c12a52c
rishithaa9 <cb.ai.u4aid23023@cb.students.amrita.edu>
2026-02-13 11:54:55 +0530

    next Greater

---
+++ 744.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        left=0
+        right=len(letters)-1
+
+        while left <= right:
+            mid=(left+right) // 2 
+
+            if letters[mid] > target:
+                right=mid-1
+            else:
+                left=mid+1
+        return letters[left % len(letters)]
