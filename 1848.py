6dde2cbbbb5664feaa19f39677be976841df61f1
rishithaa9 <cb.ai.u4aid23023@cb.students.amrita.edu>
2026-04-13 21:02:25 +0530

    Daily

---
+++ 1848.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        result=float('inf')
+        for i in range(len(nums)):
+            if nums[i]==target:
+                result=min(abs(i-start),result)
+        return result 
+        
