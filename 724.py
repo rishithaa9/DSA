248a88826bd4579c1310ea88b0aa548a293b316a
rishithaa9 <cb.ai.u4aid23023@cb.students.amrita.edu>
2026-05-07 20:47:10 +0530

    Prefix Sum

---
+++ 724.py
@@ -0,0 +1,16 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        n=len(nums)
+        ls=[0]*n 
+        rs=[0]*n
+
+        for i in range(n):
+            ls[i]=ls[i-1]+nums[i]
+        rs[-1]=nums[-1]
+        for j in range(n - 2, -1, -1):
+            rs[j] = rs[j + 1] + nums[j]
+
+        for i in range (n):
+            if ls[i]==rs[i]:
+                return i
+        return -1
