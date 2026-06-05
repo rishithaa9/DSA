33e139af9bc3f86ae52d5f58b92d18f093c82b24
rishithaa9 <cb.ai.u4aid23023@cb.students.amrita.edu>
2026-06-05 06:33:40 +0530

    Distance

---
+++ 2615_optimal.py
@@ -0,0 +1,11 @@
+class Solution:
+    def distance(self, nums: List[int]) -> List[int]:
+        arr=[0] * len(nums)
+        n=len(nums)
+        for i in range(n):
+            for j in range(n):
+                if nums[i] == nums[j] :
+                    arr[i]+=abs(i-j)
+        return arr
+
+        
