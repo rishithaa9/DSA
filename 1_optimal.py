c434e6d33ec9f83248887030e8e32484ce6943e4
rishithaa9 <cb.ai.u4aid23023@cb.students.amrita.edu>
2026-06-15 12:33:45 +0530

    twosum with hashmap

---
+++ 1_optimal.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mp={}
+        for i,num in enumerate(nums):
+            need=target-num
+            if need in mp :
+                return [mp[need],i]
+            mp[num]=i
+            
+        
