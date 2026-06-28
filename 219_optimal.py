95e93be36de1eb6f4321ba158b24f2ef083279ee
rishithaa9 <cb.ai.u4aid23023@cb.students.amrita.edu>
2026-06-28 10:49:52 +0530

    HM

---
+++ 219_optimal.py
@@ -0,0 +1,13 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        count={}
+        for i in range(len(nums)):
+            if nums[i] in count:
+                if i-count[nums[i]]<=k:
+                    return True
+            
+            count[nums[i]]=i
+
+        return False
+
+        
