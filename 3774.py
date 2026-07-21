ba8ab85dadb50df67ba55ceaf6b622891301f8ec
rishithaa9 <cb.ai.u4aid23023@cb.students.amrita.edu>
2026-07-21 18:21:41 +0530

    abs

---
+++ 3774.py
@@ -0,0 +1,8 @@
+class Solution:
+    def absDifference(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        
+        small = sum(nums[:k])
+        large = sum(nums[-k:])
+        
+        return large - small
