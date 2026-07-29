1147299bb77f07363c9bff3e074212e9d7f7b359
rishithaa9 <cb.ai.u4aid23023@cb.students.amrita.edu>
2026-07-29 14:21:01 +0530

    gcd

---
+++ DSA_Java/gcd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main {
+    public static int gcd (int a,int b) {
+        while(b!=0){
+            int temp=b;
+            b=a%b;
+            a=temp;
+        }
+        return a;
+        
+    }
+    public static void main(String[] args){
+        System.out.println(gcd(10,20));
+    }
+}
